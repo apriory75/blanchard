f8cfe89ffe268817056b85bc6aa853f5ca19e0c1
Natalya Antipova <falcon210@mail.ru>
2022-05-21 21:16:29 +0300

    adapte blanchard

--- js/index.js
+++ js/index.js
@@ -1,9 +1,7 @@
-const slider = document.querySelector('.swiper-container');
+
 const slider1 = document.querySelector('.swiper-container1');
 const slider2 = document.querySelector('.swiper-container2');
-
 const slider3 = document.querySelector('.swiper-container3');
-const slider4 = document.querySelector('.swiper-container4');
 let mySwiper1 = new Swiper(slider1, {
     slidesPerView: 3,
     slidesPerGroup: 3,
@@ -25,39 +23,34 @@ let mySwiper1 = new Swiper(slider1, {
     //   },
     loop: false,
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: '.button-next1',
+        prevEl: '.button-prev1',
     },
 
     breakpoints: {
         // when window width is >= 320px
         320: {
-            slidesPerView: 2,
-            spaceBetween: 20
+            slidesPerView: 1,
+            spaceBetween: 15
         },
         768: {
             slidesPerView: 2,
-            spaceBetween: 30
+            spaceBetween: 45
         },
-        1024: {
+        1200: {
             slidesPerView: 2,
-            spaceBetween: 35
+            spaceBetween: 45
         },
         // when window width is >= 480px
-        1400: {
-            slidesPerView: 2,
-            spaceBetween: 50
-        },
-        // when window width is >= 640px
-        1690: {
+        1600: {
             slidesPerView: 3,
             spaceBetween: 50
-        }
+        },
     }
 })
 let mySwiper2 = new Swiper(slider2, {
     slidesPerView: 3,
-    slidesPerGroup: 3,
+    slidesPerGroup: 1,
     spaceBetween: 50,
     keyboard: {
         enabled: true,
@@ -65,114 +58,76 @@ let mySwiper2 = new Swiper(slider2, {
         pageUpDown: true,
     },
 
-    pagination: {
-        el: '.pag-1',
-        type: 'fraction',
-        clickable: true,
-        slideToClickedSlide: true,
-    },
-    //   autoplay: {
-    //     delay: 5000,
-    //   },
     loop: false,
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: '.button-next2',
+        prevEl: '.button-prev2',
     },
-
-    breakpoints: {
-        // when window width is >= 320px
-        320: {
-            slidesPerView: 2,
-            spaceBetween: 20
-        },
-        768: {
-            slidesPerView: 2,
-            spaceBetween: 30
-        },
-        1024: {
-            slidesPerView: 2,
-            spaceBetween: 35
-        },
-        // when window width is >= 480px
-        1400: {
-            slidesPerView: 2,
-            spaceBetween: 50
-        },
-        // when window width is >= 640px
-        1690: {
-            slidesPerView: 3,
-            spaceBetween: 50
-        }
-    }
-})
-
-let mySwiper3 = new Swiper(slider3, {
-    slidesPerView: 3,
-    slidesPerGroup: 3,
-    spaceBetween: 50,
     pagination: {
         el: '.pag-2',
-        type: 'fraction',
+        type: 'bullets',
         clickable: true,
         slideToClickedSlide: true,
-    },
-    loop: false,
-    navigation: {
-        nextEl: '.nx1',
-        prevEl: '.pr1',
-    },
-    // autoplay: {
-    //     delay: 5000,
-    //   },
-    breakpoints: {
-        // when window width is >= 320px
-        768: {
-            slidesPerView: 2,
-            spaceBetween: 20
-        },
-        // when window width is >= 480px
-        920: {
-            slidesPerView: 2,
-            spaceBetween: 45
-        },
-        // when window width is >= 640px
-        1600: {
-            slidesPerView: 2,
-            spaceBetween: 50
-        },
-        1700: {
-            slidesPerView: 3,
-            spaceBetween: 50
-        }
-    }
-})
-let mySwiper4 = new Swiper(slider4, {
-    slidesPerView: 3,
-    slidesPerGroup: 3,
-    spaceBetween: 50,
-
-    clickable: true,
-    slideToClickedSlide: true,
-
-    loop: true,
-    navigation: {
-        nextEl: '.nx',
-        prevEl: '.pr',
-    },
+  },
     breakpoints: {
         // when window width is >= 320px
         320: {
             slidesPerView: 1,
-            spaceBetween: 20
+            spaceBetween: 20,
+        },
+    
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+        },
+    
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 25,
+        },
+        
+   
+        // when window width is >= 480px
+        1600: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+     
+        // when window width is >= 640px
+       
+    }
+})
+
+
+let mySwiper3 = new Swiper(slider3, {
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    spaceBetween: 50,
+    keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+        pageUpDown: true,
+    },
+
+    loop: false,
+    navigation: {
+        nextEl: '.button-next3',
+        prevEl: '.button-prev3',
+    },
+   
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 30
         },
         // when window width is >= 480px
-        920: {
+        660: {
             slidesPerView: 2,
             spaceBetween: 40
         },
         // when window width is >= 640px
-        1200: {
+        1024: {
             slidesPerView: 2,
             spaceBetween: 50
         },
@@ -213,9 +168,11 @@ function init() {
         center: [55.75786715778723, 37.61982277659597],
         // Уровень масштабирования. Допустимые значения:
         // от 0 (весь мир) до 19.
-        zoom: 16
+        zoom: 16,
+        scroll:false
+       
     });
-
+myMap.behaviors.disable('scrollZoom');
     // Создаём макет содержимого.
     MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
             '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
@@ -237,16 +194,17 @@ function init() {
             iconImageOffset: [-5, -38]
         }),
 
-        myPlacemarkWithContent = new ymaps.Placemark([55.75846306898368, 37.601079499999905], {
+        myPlacemarkWithContent = new ymaps.Placemark([55.75786715778723, 37.61982277659597], {
 
 
         });
 
     myMap.geoObjects
         .add(myPlacemark)
-        .add(myPlacemarkWithContent);
+      
     myMap.panes.get('ground').getElement().style.filter = 'grayscale(100%)'
 }
+
 // validation
 var selector = document.querySelector("input[type='tel']");
 var im = new Inputmask("+7 (999) 999-99-99");
@@ -308,16 +266,6 @@ Share = {
 // плавный скролл
 $(document).ready(function () {
     $("a.header-first__list-link").click(function () {
-        var elementClick = $(this).attr("href")
-        var destination = $(elementClick).offset().top;
-        jQuery("html:not(:animated),body:not(:animated)").animate({
-            scrollTop: destination
-        }, 1600);
-        return false;
-    });
-});
-$(document).ready(function () {
-    $("a.hero__first-btn").click(function () {
         var elementClick = $(this).attr("href")
         var destination = $(elementClick).offset().top;
         jQuery("html:not(:animated),body:not(:animated)").animate({
